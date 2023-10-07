9649e2cc4c75091b5c59b2a76ab2bfa819e10859
Victor Gevaert <92021746+VictorGevaert@users.noreply.github.com>
2023-10-07 15:56:20 +0200

    Update sample-app.sh

--- sample-app.sh
+++ sample-app.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 set -euo pipefail
 
-mkdir tempdir
-mkdir tempdir/templates
-mkdir tempdir/static
-
 cp sample_app.py tempdir/.
 cp -r templates/* tempdir/templates/.
 cp -r static/* tempdir/static/.
